efb5c8d9f07c7d4a5f252ac3d8e742252d74f392
jbae11 <jbae11@illinois.edu>
2018-05-10 13:44:11 -0500

    more plots for more tests

--- generate_plots.py
+++ generate_plots.py
@@ -43,6 +43,36 @@ fn = os.path.join(os.path.dirname(__file__),
 plt.savefig(fn)
 plt.close()
 
+
+test_name = 'A-dep-3'
+x = np.arange(0, 16, step=1)
+y = 0.1 * x
+plt.plot(x,y)
+plt.xlabel('Timestep')
+plt.ylabel('Power Demand [kg]')
+plt.title('Test %s Power Demand' %test_name)
+plt.axis([0, 15, min(y) - 1, max(y) + 1])
+fn = os.path.join(os.path.dirname(__file__),
+                  './docs/do/images/%s' %test_name )
+plt.grid()
+plt.savefig(fn)
+plt.close()
+
+
+test_name = 'A-dec-1'
+x = np.arange(0, 16, step=1)
+y = 2 - 0.1 * x
+plt.plot(x,y)
+plt.xlabel('Timestep')
+plt.ylabel('Power Demand [kg]')
+plt.title('Test %s Power Demand' %test_name)
+plt.axis([0, 15, min(y) - 1, max(y) + 1])
+fn = os.path.join(os.path.dirname(__file__),
+                  './docs/do/images/%s' %test_name )
+plt.savefig(fn)
+plt.grid()
+plt.close()
+
 test_name = 'A-mix-1'
 x = np.arange(0, 16, step=1)
 y = np.zeros(16)
@@ -59,6 +89,22 @@ fn = os.path.join(os.path.dirname(__file__),
 plt.savefig(fn)
 plt.close()
 
+
+test_name = 'A-mix-2'
+x = np.arange(0, 16, step=1)
+y = 0.5 * x
+y[7:] = 3 - 0.2 * (x[7:]-6)
+plt.plot(x,y)
+plt.xlabel('Timestep')
+plt.ylabel('Fuel Demand [kg]')
+plt.title('Test %s Fuel Demand' %test_name)
+plt.axis([0, 15, min(y) - 1, max(y) + 1])
+fn = os.path.join(os.path.dirname(__file__),
+                  './docs/do/images/%s' %test_name )
+plt.savefig(fn)
+plt.close()
+
+
 test_name = 'B-dep-1'
 x = np.arange(0, 16, step=1)
 y = np.zeros(16)
@@ -72,3 +118,4 @@ fn = os.path.join(os.path.dirname(__file__),
                   './docs/do/images/%s' %test_name )
 plt.savefig(fn)
 plt.close()
+
