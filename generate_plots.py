e3f7dd07c803268c1dc478426b313b7a874c158d
jbae11 <jbae11@illinois.edu>
2018-05-10 13:55:01 -0500

    dec -> mix

--- generate_plots.py
+++ generate_plots.py
@@ -61,21 +61,6 @@ plt.savefig(fn)
 plt.close()
 
 
-test_name = 'A-dec-1'
-x = np.arange(0, 16, step=1)
-y = 2 - 0.1 * x
-plt.plot(x,y)
-plt.xlabel('Timestep')
-plt.ylabel('Fuel Demand [kg]')
-plt.title('Test %s Power Demand' %test_name)
-plt.axis([0, 15, min(y) - 1, max(y) + 1])
-fn = os.path.join(os.path.dirname(__file__),
-                  './docs/do/images/%s' %test_name )
-plt.grid()
-plt.savefig(fn)
-plt.grid()
-plt.close()
-
 test_name = 'A-mix-1'
 x = np.arange(0, 16, step=1)
 y = np.zeros(16)
@@ -95,6 +80,22 @@ plt.close()
 
 
 test_name = 'A-mix-2'
+x = np.arange(0, 16, step=1)
+y = 2 - 0.1 * x
+plt.plot(x,y)
+plt.xlabel('Timestep')
+plt.ylabel('Fuel Demand [kg]')
+plt.title('Test %s Power Demand' %test_name)
+plt.axis([0, 15, min(y) - 1, max(y) + 1])
+fn = os.path.join(os.path.dirname(__file__),
+                  './docs/do/images/%s' %test_name )
+plt.grid()
+plt.savefig(fn)
+plt.grid()
+plt.close()
+
+
+test_name = 'A-mix-3'
 x = np.arange(0, 16, step=1)
 y = 0.5 * x
 y[7:] = 3 - 0.2 * (x[7:]-6)
