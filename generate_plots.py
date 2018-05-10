d857707109d44f9567aaf93a8af119521995fccc
jbae11 <jbae11@illinois.edu>
2018-05-10 13:51:21 -0500

    new images with grids

--- generate_plots.py
+++ generate_plots.py
@@ -25,6 +25,7 @@ plt.title('Test %s Fuel Demand' %test_name)
 plt.axis([0, 15, min(y) - 1, max(y) + 1])
 fn = os.path.join(os.path.dirname(__file__),
                   './docs/do/images/%s' %test_name )
+plt.grid()
 plt.savefig(fn)
 plt.close()
 
@@ -40,6 +41,7 @@ plt.title('Test %s Fuel Demand' %test_name)
 plt.axis([0, 15, min(y) - 1, max(y) + 1])
 fn = os.path.join(os.path.dirname(__file__),
                   './docs/do/images/%s' %test_name )
+plt.grid()
 plt.savefig(fn)
 plt.close()
 
@@ -49,7 +51,7 @@ x = np.arange(0, 16, step=1)
 y = 0.1 * x
 plt.plot(x,y)
 plt.xlabel('Timestep')
-plt.ylabel('Power Demand [kg]')
+plt.ylabel('Fuel Demand [kg]')
 plt.title('Test %s Power Demand' %test_name)
 plt.axis([0, 15, min(y) - 1, max(y) + 1])
 fn = os.path.join(os.path.dirname(__file__),
@@ -64,11 +66,12 @@ x = np.arange(0, 16, step=1)
 y = 2 - 0.1 * x
 plt.plot(x,y)
 plt.xlabel('Timestep')
-plt.ylabel('Power Demand [kg]')
+plt.ylabel('Fuel Demand [kg]')
 plt.title('Test %s Power Demand' %test_name)
 plt.axis([0, 15, min(y) - 1, max(y) + 1])
 fn = os.path.join(os.path.dirname(__file__),
                   './docs/do/images/%s' %test_name )
+plt.grid()
 plt.savefig(fn)
 plt.grid()
 plt.close()
@@ -86,6 +89,7 @@ plt.title('Test %s Fuel Demand' %test_name)
 plt.axis([0, 15, min(y) - 1, max(y) + 1])
 fn = os.path.join(os.path.dirname(__file__),
                   './docs/do/images/%s' %test_name )
+plt.grid()
 plt.savefig(fn)
 plt.close()
 
@@ -101,6 +105,7 @@ plt.title('Test %s Fuel Demand' %test_name)
 plt.axis([0, 15, min(y) - 1, max(y) + 1])
 fn = os.path.join(os.path.dirname(__file__),
                   './docs/do/images/%s' %test_name )
+plt.grid()
 plt.savefig(fn)
 plt.close()
 
@@ -116,6 +121,53 @@ plt.title('Test %s Power Demand' %test_name)
 plt.axis([0, 15, min(y) - 1, max(y) + 1])
 fn = os.path.join(os.path.dirname(__file__),
                   './docs/do/images/%s' %test_name )
+plt.grid()
+plt.savefig(fn)
+plt.close()
+
+
+test_name = 'B-dep-2'
+x = np.arange(0, 16, step=1)
+y = 0.1 * x
+plt.plot(x,y)
+plt.xlabel('Timestep')
+plt.ylabel('Power Demand [kg]')
+plt.title('Test %s Power Demand' %test_name)
+plt.axis([0, 15, min(y) - 1, max(y) + 1])
+fn = os.path.join(os.path.dirname(__file__),
+                  './docs/do/images/%s' %test_name )
+plt.grid()
+plt.savefig(fn)
+plt.close()
+
+
+test_name = 'B-dep-3'
+x = np.arange(0, 16, step=1)
+y = 2- 0.1 * x
+plt.plot(x,y)
+plt.xlabel('Timestep')
+plt.ylabel('Power Demand [kg]')
+plt.title('Test %s Power Demand' %test_name)
+plt.axis([0, 15, min(y) - 1, max(y) + 1])
+fn = os.path.join(os.path.dirname(__file__),
+                  './docs/do/images/%s' %test_name )
+plt.grid()
+plt.savefig(fn)
+plt.close()
+
+
+test_name = 'B-mix-1'
+x = np.arange(0, 16, step=1)
+y = 0.5 * x
+y[7:] = 3 - 0.2 * (x[7:]-6)
+plt.plot(x,y)
+plt.xlabel('Timestep')
+plt.ylabel('Power Demand [kg]')
+plt.title('Test %s Power Demand' %test_name)
+plt.axis([0, 15, min(y) - 1, max(y) + 1])
+fn = os.path.join(os.path.dirname(__file__),
+                  './docs/do/images/%s' %test_name )
+plt.grid()
 plt.savefig(fn)
 plt.close()
 
