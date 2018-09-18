a61e61a9aa727c1aca2059e27979cc5ae5a5b61b
gwenchee <gwendolynchee95@gmail.com>
2018-09-18 14:08:32 -0500

    separate file for plotting

--- docs/no/figures/plot_figures.py
+++ docs/no/figures/plot_figures.py
@@ -122,24 +122,3 @@ def plot_demand_supply(duration, demand_curve, input1,
         (commodity, test_name), bbox_inches="tight")
 
     return fig 
-
-plot_demand_supply(
-    1000,
-    'exponential',
-    10000,
-    0,
-    0,
-    'Fresh_Fuel',
-    'A-Constant-1',
-    True)
-plot_demand_supply(1000, 'linear', 100, 0, 0, 'Power', 'A-Growth-1', True)
-plot_demand_supply(
-    1000,
-    'exponential',
-    10,
-    0.1,
-    0,
-    'Power',
-    'A-Growth-2',
-    True)
-plot_demand_supply(10, 'stepwise', 10, 20, 5, 'Power', 'A-Growth-3', True)
